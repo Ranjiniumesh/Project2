bc3809d17229ffe7a68fbbf22cafe426f4d3b3bb
Ubuntu <ubuntu@ip-172-31-47-79.ap-south-1.compute.internal>
2024-01-11 11:39:17 +0000

    sum file added

---
+++ sum.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "enter the num to find the sum of first n numbers"
+read num
+sum=0
+while [ $num -gt 0 ]
+do
+	sum=`expr $num + $sum`
+	num=`expr $num - 1`
+done
+echo " the sum of first n number is $sum"
